9228381b05e1bcf8ea0dd249c7713ea3fc0c1256
Teddy <jfrancoisteddy@gmail.com>
2021-03-26 23:10:36 +0100

    Routes initialization

---
+++ routes/pass.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import {PassController} from '../controllers/pass.controller';
+import {DatabaseUtils} from "../config/db.config";
+
+const router = express.Router();
+
+router.get("/", async function(req, res) {
+});
+
+router.post("/", async function(req, res) {
+});
+
+router.put("/", async function(req, res) {
+});
+
+router.delete("/", async function(req, res) {
+});
+
+export default router;
