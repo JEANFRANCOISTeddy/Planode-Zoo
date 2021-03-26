c5880d1e0ebc070cf4f8309effac311c1caf5cdc
Teddy <jfrancoisteddy@gmail.com>
2021-03-26 23:11:51 +0100

    Database config added

---
+++ config/db.config.ts
@@ -0,0 +1,20 @@
+import {Connection, createConnection} from "mysql2/promise";
+
+export class DatabaseUtils{
+
+    private static connection?: Connection;
+
+    static async getConnection(): Promise<Connection> {
+        if(!DatabaseUtils.connection) {
+            DatabaseUtils.connection = await createConnection({
+                host: 'localhost',
+                user: 'root',
+                password: '',
+                database: 'planodezoo',
+                port: 3306
+            })
+        }
+        return DatabaseUtils.connection;
+    }
+
+}
