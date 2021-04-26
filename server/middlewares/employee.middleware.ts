47ef37a681ad876dd964d7a74aec6f1a10df4a94
Teddy <jfrancoisteddy@gmail.com>
2021-04-26 11:03:40 +0200

    Last verification

---
+++ server/middlewares/employee.middleware.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import {UserController} from "../controllers/user.controller";
+
+export async function employeeMiddleware(req: express.Request, res: express.Response, next: express.NextFunction) {
+    const auth = req.headers["authorization"];
+    if(auth !== undefined) {
+        const token = auth.slice(7);
+        const userController = await UserController.getInstance();
+        const session = await userController.getSession(token);
+        if(session !== null) {
+            const user = await session.getUser();
+            if(user.role !== "visitor"){
+                next();
+                return;
+            }else{
+                res.status(401).end();
+                return;
+            }
+        } else {
+            res.status(403).end();
+            return;
+        }
+    } else {
+        res.status(401).end();
+    }
+}
